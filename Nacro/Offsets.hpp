e0f2eea930316bcc80168fd8125a8130bcc0eebb
ozne <68667854+Pakchunk@users.noreply.github.com>
2021-08-08 18:54:01 -0400

    ACTUALLY BETTER ORGANIZED NOW

--- Nacro/Offsets.hpp
+++ Nacro/Offsets.hpp
@@ -4,15 +4,15 @@
 
 namespace Offsets
 {
-	uintptr_t GEngineOffset 			=	0x674AB20;		//Gloabal Engine*
-	uintptr_t GWorldOffset				=	0x674CD00;		//Global World*
-	uintptr_t GNamesOffset 				=	0x66587C8;		//Global NameArray*
-	uintptr_t GUObjectArrayOffset 		=	0x6661380;		//Global ObjectArray-Class*
-	uintptr_t TUObjectArrayOffset 		=	0x6661390;		//Global ObjectArray*
-	uintptr_t ProcessEventOffset 		=	0x13D86E0;		//UObject::ProcessEvent()
-	uintptr_t SCO_IOffset 				=	0x13DD730;		//StaticContructObject_Internal()
-	uintptr_t SLO_IOffset 				=	0x13E0180;		//StaticLoadObject_Internal()
-	uintptr_t SpawnActorOffset 			=	0x137DBC0;		//UWorld::SpawnActor()
-	uintptr_t PlainNameStringOffset 	=	0x12F0FC0;		//FNameEntry::GetPlainNameString()
-	uintptr_t CGInternalOffset			=	0x137D380;		//CollectGarbageInternal()
+	uintptr_t GEngineOffset 		=	0x674AB20;	//Gloabal Engine*
+	uintptr_t GWorldOffset			=	0x674CD00;	//Global World*
+	uintptr_t GNamesOffset 			=	0x66587C8;	//Global NameArray*
+	uintptr_t GUObjectArrayOffset 		=	0x6661380;	//Global ObjectArray-Class*
+	uintptr_t TUObjectArrayOffset 		=	0x6661390;	//Global ObjectArray*
+	uintptr_t ProcessEventOffset 		=	0x13D86E0;	//UObject::ProcessEvent()
+	uintptr_t SCO_IOffset 			=	0x13DD730;	//StaticContructObject_Internal()
+	uintptr_t SLO_IOffset 			=	0x13E0180;	//StaticLoadObject_Internal()
+	uintptr_t SpawnActorOffset 		=	0x137DBC0;	//UWorld::SpawnActor()
+	uintptr_t PlainNameStringOffset 	=	0x12F0FC0;	//FNameEntry::GetPlainNameString()
+	uintptr_t CGInternalOffset		=	0x137D380;	//CollectGarbageInternal()
 };
